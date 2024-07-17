d446f8d1c2bd5f88e3f71f20a7b953092d2b54c2
kimyoda <rladygks1210@naver.com>
2024-07-18 02:52:36 +0900

    [feat] TodoList.tsx, TodoListItem.tsx add

--- typescript-todo-tutorial/src/components/TodoForm.tsx
+++ typescript-todo-tutorial/src/components/TodoForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { ChangeEvent, FormEvent, useState } from "react";
 
 interface TodoFormProps {
   addTodo: AddTodo;
@@ -7,6 +7,18 @@ interface TodoFormProps {
 export const TodoForm: React.FC<TodoFormProps> = ({ addTodo }) => {
   const [newTodo, setNewTodo] = useState<string>("");
 
+  // input칸에 입력값, onChange 이벤트
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setNewTodo(e.target.value);
+  };
+
+  // 버튼을 눌렀을 때 FormEvenet가 발생되어 새로운 Todo 추가 이벤트
+  const handleSubmit = (e: FormEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    addTodo(newTodo);
+    setNewTodo("");
+  };
+
   return (
     <form className="todo-form">
       <input
@@ -14,7 +26,11 @@ export const TodoForm: React.FC<TodoFormProps> = ({ addTodo }) => {
         value={newTodo}
         className="todo-input"
         placeholder="투두를 입력해주세요"
+        onChange={handleChange}
       />
+      <button type="submit" className="todo-button" onClick={handleSubmit}>
+        Add a todo
+      </button>
     </form>
   );
 };
